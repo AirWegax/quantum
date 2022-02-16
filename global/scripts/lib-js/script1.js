d6d85e984c51ac0f3680c39ba85b2a49dcd21999
Reznix <96864786+Reznix@users.noreply.github.com>
2022-02-16 08:50:39 +0600

    scripts ++
    script1 to script7 for library-day

---
+++ global/scripts/lib-js/script1.js
@@ -0,0 +1,40 @@
+//varibalse today-tomorow and more..
+let date = new Date();
+
+let month = date.getMonth();
+let day = date.getDate();
+let dayWeek = date.getDay();
+
+//logick change date
+
+month = month + 1;
+
+if (dayWeek == 0) {
+	day = date.getDate();
+	day = day - 6;
+	document.title = 'Quantum ' + '| ' + 'ПН ' + month + '/' + day;
+} else if (dayWeek == 1) {
+	day = date.getDate();
+	day = day;
+	document.title = 'Quantum ' + '| ' + 'ПН ' + month + '/' + day;	
+} else if (dayWeek == 2) {
+	day = date.getDate();
+	day = day - 1;
+	document.title = 'Quantum ' + '| ' + 'ПН ' + month + '/' + day;	
+} else if (dayWeek == 3) {
+	day = date.getDate();
+	day = day - 2;
+	document.title = 'Quantum ' + '| ' + 'ПН ' + month + '/' + day;	
+} else if (dayWeek == 4) {
+	day = date.getDate();
+	day = day - 3;
+	document.title = 'Quantum ' + '| ' + 'ПН ' + month + '/' + day;	
+} else if (dayWeek == 5) {
+	day = date.getDate();
+	day = day - 4;
+	document.title = 'Quantum ' + '| ' + 'ПН ' + month + '/' + day;	
+} else if (dayWeek == 6) {
+	day = date.getDate();
+	day = day - 5;
+	document.title = 'Quantum ' + '| ' + 'ПН ' + month + '/' + day;	
+}
---
+++ global/scripts/lib-js/script5.js
@@ -0,0 +1,38 @@
+//varibalse today-tomorow and more..
+let date = new Date();
+
+let month = date.getMonth();
+let day = date.getDate();
+let dayWeek = date.getDay();
+
+//logick change date
+
+month = month + 1;
+
+if (dayWeek == 0) {
+	day = day - 2;
+} else if (dayWeek == 1) {
+	day = date.getDate();
+	day = day + 4;
+	document.title = 'Quantum ' + '| ' + 'ПТ ' + month + '/' + day;	
+} else if (dayWeek == 2) {
+	day = date.getDate();
+	day = day + 3;
+	document.title = 'Quantum ' + '| ' + 'ПТ ' + month + '/' + day;	
+} else if (dayWeek == 3) {
+	day = date.getDate();
+	day = day + 2;
+	document.title = 'Quantum ' + '| ' + 'ПТ ' + month + '/' + day;	
+} else if (dayWeek == 4) {
+	day = date.getDate();
+	day = day + 1;
+	document.title = 'Quantum ' + '| ' + 'ПТ ' + month + '/' + day;	
+} else if (dayWeek == 5) {
+	day = date.getDate();
+	day = day;
+	document.title = 'Quantum ' + '| ' + 'ПТ ' + month + '/' + day;	
+} else if (dayWeek == 6) {
+	day = date.getDate();
+	day = day - 1;
+	document.title = 'Quantum ' + '| ' + 'ПТ ' + month + '/' + day;	
+}
