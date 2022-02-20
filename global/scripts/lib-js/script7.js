88a50923f0d995afdcd26660e246b5b926222dd5
Reznix <96864786+Reznix@users.noreply.github.com>
2022-02-20 11:02:51 +0600

    Add files via upload

--- global/scripts/lib-js/script7.js
+++ global/scripts/lib-js/script7.js
@@ -13,28 +13,239 @@ month = month + 1;
 if (dayWeek == 0) {
 	day = date.getDate();
 	day = day;
+	if (month == 1 && day >= 31) {
+		month = month + 1;
+	} else if(month == 2) {
+		if (day >= 28) {
+			month = 3;
+		}
+		if (day >= 29) {
+			month = 3;
+		}
+	} else if(month == 3 && day >= 31) {
+		month = month + 1;
+	} else if(month == 4 && day >= 30) {
+		month = month + 1;
+	} else if(month == 5 && day >= 31) {
+		month = month + 1;
+	} else if(month == 6 && day >= 30) {
+		month = month + 1;
+	} else if(month == 7 && day >= 31) {
+		month = month + 1;
+	} else if(month == 8 && day >= 31) {
+		month = month + 1;
+	} else if(month == 9 && day >= 30) {
+		month = month + 1;
+	} else if(month == 10 && day >= 31) {
+		month = month + 1;
+	} else if(month == 11 && day >= 30) {
+		month = month + 1;
+	} else if(month == 12 && day >= 31) {
+		month = month + 1;
+	}
+	document.title = 'Quantum ' + '| ' + 'ВС ' + month + '/' + day;
 } else if (dayWeek == 1) {
 	day = date.getDate();
 	day = day + 1;
+	if (month == 1 && day >= 31) {
+		month = month + 1;
+	} else if(month == 2) {
+		if (day >= 28) {
+			month = 3;
+		}
+		if (day >= 29) {
+			month = 3;
+		}
+	} else if(month == 3 && day >= 31) {
+		month = month + 1;
+	} else if(month == 4 && day >= 30) {
+		month = month + 1;
+	} else if(month == 5 && day >= 31) {
+		month = month + 1;
+	} else if(month == 6 && day >= 30) {
+		month = month + 1;
+	} else if(month == 7 && day >= 31) {
+		month = month + 1;
+	} else if(month == 8 && day >= 31) {
+		month = month + 1;
+	} else if(month == 9 && day >= 30) {
+		month = month + 1;
+	} else if(month == 10 && day >= 31) {
+		month = month + 1;
+	} else if(month == 11 && day >= 30) {
+		month = month + 1;
+	} else if(month == 12 && day >= 31) {
+		month = month + 1;
+	}
 	document.title = 'Quantum ' + '| ' + 'ВС ' + month + '/' + day;	
 } else if (dayWeek == 2) {
 	day = date.getDate();
 	day = day + 5;
+	if (month == 1 && day >= 31) {
+		month = month + 1;
+	} else if(month == 2) {
+		if (day >= 28) {
+			month = 3;
+		}
+		if (day >= 29) {
+			month = 3;
+		}
+	} else if(month == 3 && day >= 31) {
+		month = month + 1;
+	} else if(month == 4 && day >= 30) {
+		month = month + 1;
+	} else if(month == 5 && day >= 31) {
+		month = month + 1;
+	} else if(month == 6 && day >= 30) {
+		month = month + 1;
+	} else if(month == 7 && day >= 31) {
+		month = month + 1;
+	} else if(month == 8 && day >= 31) {
+		month = month + 1;
+	} else if(month == 9 && day >= 30) {
+		month = month + 1;
+	} else if(month == 10 && day >= 31) {
+		month = month + 1;
+	} else if(month == 11 && day >= 30) {
+		month = month + 1;
+	} else if(month == 12 && day >= 31) {
+		month = month + 1;
+	}
 	document.title = 'Quantum ' + '| ' + 'ВС ' + month + '/' + day;	
 } else if (dayWeek == 3) {
 	day = date.getDate();
 	day = day + 4;
+	if (month == 1 && day >= 31) {
+		month = month + 1;
+	} else if(month == 2) {
+		if (day >= 28) {
+			month = 3;
+		}
+		if (day >= 29) {
+			month = 3;
+		}
+	} else if(month == 3 && day >= 31) {
+		month = month + 1;
+	} else if(month == 4 && day >= 30) {
+		month = month + 1;
+	} else if(month == 5 && day >= 31) {
+		month = month + 1;
+	} else if(month == 6 && day >= 30) {
+		month = month + 1;
+	} else if(month == 7 && day >= 31) {
+		month = month + 1;
+	} else if(month == 8 && day >= 31) {
+		month = month + 1;
+	} else if(month == 9 && day >= 30) {
+		month = month + 1;
+	} else if(month == 10 && day >= 31) {
+		month = month + 1;
+	} else if(month == 11 && day >= 30) {
+		month = month + 1;
+	} else if(month == 12 && day >= 31) {
+		month = month + 1;
+	}
 	document.title = 'Quantum ' + '| ' + 'ВС ' + month + '/' + day;	
 } else if (dayWeek == 4) {
 	day = date.getDate();
 	day = day + 3;
+	if (month == 1 && day >= 31) {
+		month = month + 1;
+	} else if(month == 2) {
+		if (day >= 28) {
+			month = 3;
+		}
+		if (day >= 29) {
+			month = 3;
+		}
+	} else if(month == 3 && day >= 31) {
+		month = month + 1;
+	} else if(month == 4 && day >= 30) {
+		month = month + 1;
+	} else if(month == 5 && day >= 31) {
+		month = month + 1;
+	} else if(month == 6 && day >= 30) {
+		month = month + 1;
+	} else if(month == 7 && day >= 31) {
+		month = month + 1;
+	} else if(month == 8 && day >= 31) {
+		month = month + 1;
+	} else if(month == 9 && day >= 30) {
+		month = month + 1;
+	} else if(month == 10 && day >= 31) {
+		month = month + 1;
+	} else if(month == 11 && day >= 30) {
+		month = month + 1;
+	} else if(month == 12 && day >= 31) {
+		month = month + 1;
+	}
 	document.title = 'Quantum ' + '| ' + 'ВС ' + month + '/' + day;	
 } else if (dayWeek == 5) {
 	day = date.getDate();
 	day = day + 2;
+	if (month == 1 && day >= 31) {
+		month = month + 1;
+	} else if(month == 2) {
+		if (day >= 28) {
+			month = 3;
+		}
+		if (day >= 29) {
+			month = 3;
+		}
+	} else if(month == 3 && day >= 31) {
+		month = month + 1;
+	} else if(month == 4 && day >= 30) {
+		month = month + 1;
+	} else if(month == 5 && day >= 31) {
+		month = month + 1;
+	} else if(month == 6 && day >= 30) {
+		month = month + 1;
+	} else if(month == 7 && day >= 31) {
+		month = month + 1;
+	} else if(month == 8 && day >= 31) {
+		month = month + 1;
+	} else if(month == 9 && day >= 30) {
+		month = month + 1;
+	} else if(month == 10 && day >= 31) {
+		month = month + 1;
+	} else if(month == 11 && day >= 30) {
+		month = month + 1;
+	} else if(month == 12 && day >= 31) {
+		month = month + 1;
+	}
 	document.title = 'Quantum ' + '| ' + 'ВС ' + month + '/' + day;	
 } else if (dayWeek == 6) {
 	day = date.getDate();
 	day = day + 1;
+	if (month == 1 && day >= 31) {
+		month = month + 1;
+	} else if(month == 2) {
+		if (day >= 28) {
+			month = 3;
+		}
+		if (day >= 29) {
+			month = 3;
+		}
+	} else if(month == 3 && day >= 31) {
+		month = month + 1;
+	} else if(month == 4 && day >= 30) {
+		month = month + 1;
+	} else if(month == 5 && day >= 31) {
+		month = month + 1;
+	} else if(month == 6 && day >= 30) {
+		month = month + 1;
+	} else if(month == 7 && day >= 31) {
+		month = month + 1;
+	} else if(month == 8 && day >= 31) {
+		month = month + 1;
+	} else if(month == 9 && day >= 30) {
+		month = month + 1;
+	} else if(month == 10 && day >= 31) {
+		month = month + 1;
+	} else if(month == 11 && day >= 30) {
+		month = month + 1;
+	} else if(month == 12 && day >= 31) {
+		month = month + 1;
+	}
 	document.title = 'Quantum ' + '| ' + 'ВС ' + month + '/' + day;	
 }
