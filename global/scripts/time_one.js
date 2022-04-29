1edd5c92cccedfab655542339bf6e73a3e4d0e11
Reznix <srt.reznix@gmail.com>
2022-04-29 21:00:08 +0600

    commit X

--- global/scripts/time_one.js
+++ global/scripts/time_one.js
@@ -1,11 +1,10 @@
-const timeTimer = document.getElementById('timetimer')
+const timeTimer = document.getElementById('time-timer')
+const lessonLeft = document.getElementById('lesson-left')
 
 let date = new Date();
 let hour = date.getHours();
 let min = date.getMinutes();
 let day = date.getDay();
-
-let minTimer = 0;
 
 function datesUpdated() {
 	date = new Date();
@@ -15,13 +14,15 @@ function datesUpdated() {
 }
 setInterval(datesUpdated, 1000)
 
-if ((day == 0) || (day == 6)) {
+let minTimer = 0;
+
+if ((day === 0) || (day === 6)) {
 	timeTimer.textContent = 'Выходные!';
 }
 
 function timeUpdate() {
 	if (day >= 1 && day <= 5) {
-		if (hour == 7) {
+		if (hour === 7) {
 			minTimer = 60;
 			minTimer = minTimer - min;
 			timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
@@ -29,136 +30,170 @@ function timeUpdate() {
 		}
 		if (hour >= 8 && hour <= 13) {
 			// first lesson
-			if (hour == 8 && min <= 45) {
+			if (hour === 8 && min <= 45) {
+				// lesson left: 7
 				minTimer = 45;
 				minTimer = minTimer - min;
 				timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (7)';
 				minTimer = 0;
 			}
-			if (hour == 8) {
+			// turn
+			if (hour === 8) {
 				if (min >= 45 && min <= 50) {
+					// lesson left: 6
 					minTimer = 50;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (6)';
 					minTimer = 0;
 				}
 			}
 			// second lesson
-			if ((hour == 8) || (hour == 9)) {
+			if ((hour === 8) || (hour === 9)) {
+				// lesson left: 6
 				if (min >= 50) {
 					minTimer = 95;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (6)';
 					minTimer = 0;
-				} else if (hour == 9 && min <= 35) {
+				} else if (hour === 9 && min <= 35) {
 					minTimer = 35;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (6)';
 					minTimer = 0;	
 				}
 			}
-			if (hour == 9) {
+			// lesson left: 5
+			// turn
+			if (hour === 9) {
 				if (min >= 35 && min <= 45) {
 					minTimer = 45;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (5)';
 					minTimer = 0;
 				}
 			}
 			// third lesson
-			if ((hour == 9) || (hour == 10)) {
+			if ((hour === 9) || (hour === 10)) {
+				// lesson left: 5
 				if (min >= 45) {
 					minTimer = 90;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (5)';
 					minTimer = 0;
-				} else if (hour == 10 && min <= 30) {
+				} else if (hour === 10 && min <= 30) {
 					minTimer = 30;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (5)';
 					minTimer = 0;	
 				}
 			}
-			if (hour == 10) {
+			if (hour === 10) {
+				// lesson left: 4
+				// turn
 				if (min >= 30 && min <= 35) {
 					minTimer = 35;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (4)';
 					minTimer = 0;
 				}
 			}
 			// fourth lesson
-			if ((hour == 10) || (hour == 11)) {
+			if ((hour === 10) || (hour === 11)) {
+				// lesson left: 4
 				if (min >= 35) {
 					minTimer = 80;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (4)';
 					minTimer = 0;
-				} else if (hour == 11 && min <= 20) {
+				} else if (hour === 11 && min <= 20) {
 					minTimer = 20;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (4)';
 					minTimer = 0;	
 				}
 			}
-			if (hour == 11) {
+			if (hour === 11) {
+				// lesson left: 3
 				if (min >= 20 && min <= 30) {
 					minTimer = 30;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (3)';
 					minTimer = 0;
 				}
 			}
 			// fifth lesson
-			if ((hour == 11) || (hour == 12)) {
+			if ((hour === 11) || (hour === 12)) {
+				// lesson left: 3
 				if (min >= 30) {
 					minTimer = 75;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (3)';
 					minTimer = 0;
-				} else if (hour == 12 && min <= 15) {
+				} else if (hour === 12 && min <= 15) {
 					minTimer = 15;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (3)';
 					minTimer = 0;	
 				}
 			}
-			if (hour == 12) {
+			if (hour === 12) {
+				// lesson left: 2
 				if (min >= 15 && min <= 20) {
 					minTimer = 20;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (2)';
 					minTimer = 0;
 				}
 			}
 			// sixth lesson
-			if ((hour == 12) || (hour == 13)) {
-				if (min >= 20) {
+			if ((hour === 12) || (hour === 13)) {
+				// lesson left: 2
+				if (hour === 12 && min >= 20) {
 					minTimer = 65;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (2)';
 					minTimer = 0;
-				} else if (hour == 13 && min <= 5) {
+				} else if (hour === 13 && min <= 5) {
 					minTimer = 5;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (2)';
 					minTimer = 0;	
 				}
 			}
-			if (hour == 13) {
+			if (hour === 13) {
+				// lesson left: 1
 				if (min >= 5 && min <= 10) {
 					minTimer = 10;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (1)';
 					minTimer = 0;
 				}
 			}
 			// seventh lesson
-			if (hour == 13 && min >= 10) {
-				if (min <= 55) {
+			if (hour === 13 && min >= 10) {
+				// lesson left: 1
+				if (min < 55) {
 					minTimer = 55;
 					minTimer = minTimer - min;
 					timeTimer.textContent = 'До конца: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (1)';
 					minTimer = 0;
 				}
 			}
--- global/scripts/time_two.js
+++ global/scripts/time_two.js
@@ -1,10 +1,13 @@
 // start
-const timeTimer = document.getElementById('timetimer')
+const timeTimer = document.getElementById('time-timer')
+const lessonLeft = document.getElementById('lesson-left')
 
 let date = new Date();
 let hour = date.getHours();
 let min = date.getMinutes();
 let day = date.getDay();
+
+let minTimer = 75;
 
 function datesUpdated() {
 	date = new Date();
@@ -13,138 +16,165 @@ function datesUpdated() {
 	day = date.getDay();
 }
 setInterval(datesUpdated, 1000)
-let minTimer = 75;
 
-if ((day == 0) || (day == 6)) {
+if ((day === 0) || (day === 6)) {
 	timeTimer.textContent = 'Выходные!';
 }
 function timeUpdate() {
 	if (day >= 1 && day <= 5) {
-		if ((hour == 13) || (hour == 14)) {
+		if ((hour === 13) || (hour === 14)) {
 			minTimer = minTimer - min;
-			if (hour == 13) {
+			if (hour === 13) {
 				if (minTimer <= 75 && minTimer >= 60) {
-					let timerHour = 0;
+					let timerHour;
 					timerHour = minTimer - 60;
-					timeTimer.textContent = 'До урока: ' + '1 час ' + timerHour + ' минут';
+					timeTimer.textContent = 'До (1-го) урока: ' + '1 час ' + timerHour + ' минут';
 				} else {
-					timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+					timeTimer.textContent = 'До (1-го) урока: ' + minTimer + ' минут';
 				}
 			}
-			if (hour == 14) {
+			if (hour === 14) {
 				minTimer = 15;
 				minTimer = minTimer - min;
-				timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+				timeTimer.textContent = 'До (1-го) урока: ' + minTimer + ' минут';
 				minTimer = 0;
 			}
 		}
 		// first lesson
-	 	if ((hour == 14) || (hour == 15)) {
+	 	if ((hour === 14) || (hour === 15)) {
+			// lesson left: 6
 	 		if (min >= 15) {
 	 			minTimer = 60;
 	 			minTimer = minTimer - min;
 	 			timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (6)';
 	 			minTimer = 0;
-	 		} else if (hour == 15 && min <= 0) {
+	 		} else if (hour === 15 && min <= 0) {
 	 			minTimer = 0;
 	 			minTimer = minTimer - min;
 	 			timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (6)';
 	 			minTimer = 0;	
 	 		}
-	 		if (hour == 15) {
+			 // turn
+	 		if (hour === 15) {
 	 			if (min >= 0 && min <= 5) {
+					// lesson left: 5
 		 			minTimer = 5;
 		 			minTimer = minTimer - min;
 		 			timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+					lessonLeft.textContent = 'Осталось уроков: (5)';
 		 			minTimer = 0;
 	 			}
 			}
 		}
 		// second lesson
-		if (hour == 15 && min >= 5) {
+		if (hour === 15 && min >= 5) {
+			// lesson left: 5
 		 	minTimer = 50;
 			minTimer = minTimer - min;
 			timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+			lessonLeft.textContent = 'Осталось уроков: (5)';
 			minTimer = 0;
 		}
-		if ((hour == 15) || (hour == 16)) {
+		// turn
+		if ((hour === 15) || (hour === 16)) {
+			// lesson left: 4
 		 	if (min >= 50) {
 	 			minTimer = 60;
 	 			minTimer = minTimer - min;
 	 			timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (4)';
 	 			minTimer = 0;
 			} 
 		}
 		// third lesson
-		if (hour == 16 && min >= 0) {
+		if (hour === 16 && min >= 0) {
+			// lesson left: 4
 		 	minTimer = 45;
 			minTimer = minTimer - min;
 			timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+			lessonLeft.textContent = 'Осталось уроков: (4)';
 			minTimer = 0;
 		}
-		if (hour == 16) {
+		if (hour === 16) {
 			if (min >= 45 && min <= 55) {
+				// lesson left: 3
 	 			minTimer = 55;
 	 			minTimer = minTimer - min;
 	 			timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (3)';
 	 			minTimer = 0;
 			}
 		}
 		// fourth lesson
-		if ((hour == 16) || (hour == 17)) {
+		if ((hour === 16) || (hour === 17)) {
+			// lesson left: 3
 			if (min >= 55) {
 				minTimer = 100;
 				minTimer = minTimer - min;
 				timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (3)';
 				minTimer = 0;
-			} else if (hour == 17 && min <= 40) {
+			} else if (hour === 17 && min <= 40) {
 				minTimer = 40;
 				minTimer = minTimer - min;
 				timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (3)';
 				minTimer = 0;	
 			}
 		}
-		if (hour == 17) {
+		if (hour === 17) {
 			if (min >= 40 && min <= 45) {
+				// lesson left: 2
 				minTimer = 45;
 				minTimer = minTimer - min;
 				timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (2)';
 				minTimer = 0;
 			}
 		}
 		// fifth lesson
-		if ((hour == 17) || (hour == 18)) {
+		if ((hour === 17) || (hour === 18)) {
+			// lesson left: 2
 			if (min >= 45) {
 				minTimer = 90;
 				minTimer = minTimer - min;
 				timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (2)';
 				minTimer = 0;
-			} else if (hour == 18 && min <= 30) {
+			} else if (hour === 18 && min <= 30) {
 				minTimer = 30;
 				minTimer = minTimer - min;
 				timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (2)';
 				minTimer = 0;	
 			}
 		}
-		if (hour == 18) {
+		if (hour === 18) {
 			if (min >= 30 && min <= 35) {
+				// lesson left: 1
 				minTimer = 35;
 				minTimer = minTimer - min;
 				timeTimer.textContent = 'До урока: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (1)';
 				minTimer = 0;
 			}
 		}
 		// sixth lesson
-		if ((hour == 18) || (hour == 19)) {
+		if ((hour === 18) || (hour === 19)) {
+			// lesson left: 1
 			if (min >= 35) {
 				minTimer = 80;
 				minTimer = minTimer - min;
 				timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (1)';
 				minTimer = 0;
-			} else if (hour == 19 && min <= 20) {
+			} else if (hour === 19 && min < 20) {
 				minTimer = 20;
 				minTimer = minTimer - min;
 				timeTimer.textContent = 'До перемены: ' + minTimer + ' минут';
+				lessonLeft.textContent = 'Осталось уроков: (1)';
 				minTimer = 0;	
 			}
 		}
